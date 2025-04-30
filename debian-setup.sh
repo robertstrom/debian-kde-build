350fd206dda2a4e1e86b332b9ab3ddf60d78496d
robertstrom <robert.strom@gmail.com>
2025-04-30 11:44:25 -0700

    update x86/x64 programs to upstall
    removed the more Kali specific programs that were from the Kali build script

--- debian-setup.sh
+++ debian-setup.sh
@@ -48,12 +48,12 @@ arch=$(uname -m)
 
 case "$arch" in
   x86_64|amd64)
-    sudo DEBIAN_FRONTEND=noninteractive apt install -yq shellcheck libimage-exiftool-perl pv terminator copyq xclip dolphin krusader kdiff3 krename kompare xxdiff krename kde-spectacle \
-    flameshot html2text csvkit remmina kali-wallpapers-all hollywood-activate kali-screensaver gridsite-clients shellter sipcalc \
-    xsltproc rinetd torbrowser-launcher httptunnel kerberoast tesseract-ocr ncdu grepcidr speedtest-cli sshuttle mpack filezilla lolcat \
-    ripgrep bat dcfldd redis-tools feroxbuster name-that-hash jq keepassxc okular exfat-fuse exfatprogs kate xsel pandoc poppler-utils ffmpeg \
-    zbar-tools gnupg2 dc3dd rlwrap partitionmanager kali-undercover fastfetch hyfetch lolcat 7zip-standalone eza autorecon docker.io docker-cli \
-    code-oss obsidian breeze-icon-theme trufflehog python3-trufflehogregexes coercer golang-go ligolo-ng sublist3r tcpspy xrdp mono-complete
+    sudo DEBIAN_FRONTEND=noninteractive apt install -yq shellcheck libimage-exiftool-perl pv terminator copyq xclip dolphin krusader \
+    flameshot html2text csvkit remmina sipcalc \
+    xsltproc rinetd torbrowser-launcher httptunnel tesseract-ocr ncdu grepcidr speedtest-cli sshuttle mpack filezilla lolcat \
+    ripgrep bat dcfldd redis-tools jq keepassxc okular exfat-fuse exfatprogs xsel pandoc poppler-utils ffmpeg \
+    zbar-tools gnupg2 dc3dd rlwrap fastfetch hyfetch lolcat 7zip-standalone eza docker.io docker-cli \
+    code-oss obsidian trufflehog python3-trufflehogregexes golang-go ligolo-ng sublist3r tcpspy xrdp mono-complete
     ;;
   i?86)
     echo "Architecture: x86 (32-bit)"
