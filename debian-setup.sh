76f774387c5f281c17719ea4ea6f09b97a8890b8
robertstrom <robert.strom@gmail.com>
2025-05-02 00:02:20 -0700

    added alternative command to modify the ohmyzsh plugins

--- debian-setup.sh
+++ debian-setup.sh
@@ -265,6 +265,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Change zsh theme to agnoster
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' .zshrc
+### Command to test differnet way to modify the ohmyzsh plugins
+## sed -i 's/plugins=(git)/plugins=(git\nautojump\ncolored-man-pages\ncolorize\ncopyfile\ncopypath\nfzf\neza)/'
 sed -i 's/plugins=(git)/plugins=(git colored-man-pages colorize copyfile copypath fzf eza)/'
 
 # Install fzf via github
