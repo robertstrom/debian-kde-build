cc1a21ac84f9c7153f9cd1078c4afbf13e72d019
robertstrom <robert.strom@gmail.com>
2025-05-03 14:58:36 -0700

    Added mc (Midnight Commander)

--- debian-setup.sh
+++ debian-setup.sh
@@ -78,7 +78,7 @@ case "$arch" in
     ffmpeg fonts-liberation zbar-tools dc3dd rlwrap lolcat 7zip pip virtualenv python3-virtualenv pipx \
     golang sublist3r tcpspy mono-complete zsh qemu-system-x86 libvirt-daemon-system virtinst virt-manager virt-viewer ovmf swtpm \
     qemu-utils guestfs-tools libosinfo-bin tuned fonts-powerline autojump htop glances btop ncdu vlc stacer audacity obs-studio handbrake handbrake-cli \
-    slack docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin torbrowser-launcher apt-transport-https
+    slack docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin torbrowser-launcher apt-transport-https mc
     ;;
   i?86)
     echo "Architecture: x86 (32-bit)"
