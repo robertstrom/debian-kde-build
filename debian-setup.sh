dc7baaf200dd42fead0ccefe37ca7a39d0739fe5
robertstrom <robert.strom@gmail.com>
2025-05-03 23:13:24 -0700

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -310,7 +310,7 @@ export PATH="/home/rstrom/go/bin/":$PATH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Change zsh theme to agnoster
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' /.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
 ### Command to test differnet way to modify the ohmyzsh plugins
 ## sed -i 's/plugins=(git)/plugins=(git\nautojump\ncolored-man-pages\ncolorize\ncopyfile\ncopypath\nfzf\neza)/'
 sed -i 's/plugins=(git)/plugins=(git colored-man-pages colorize copyfile copypath fzf eza)/' ~/.zshrc
